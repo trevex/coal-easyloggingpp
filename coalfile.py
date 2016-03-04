4a28153e4834ead092570b41ff1c21fd4acfda6f
Nik Voss <niklas.voss@gmail.com>
2016-03-04 10:47:44 +0100

    Basic coalfile for easylogging dep.

---
+++ coalfile.py
@@ -0,0 +1,15 @@
+from coal import CoalFile
+from util import git_clone, download, unzip, default_cmake_build, cp
+from os import path
+
+class EasyloggingppFile(CoalFile):
+    url = "https://github.com/easylogging/easyloggingpp"
+    exports = ["include"]
+    def prepare(self):
+        git_clone(self.url, 'master', 'src')
+    def build(self):
+        pass
+    def package(self):
+        cp('build/src', 'include')
+    def info(self, generator):
+        generator.add_include_dir('include/')
